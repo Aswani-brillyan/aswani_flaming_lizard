469778c4f42f756872f2f3b4bfdb2c39144f4edf
Aswani-brillyan <brillyanaswani@gmail.com>
2026-03-20 22:33:16 +0300

    Update NavbarComponent.jsx

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -7,4 +7,44 @@ const navbarComponent = () => {
      );
 }
  
-export default navbarComponent;
+expimport { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const user = JSON.parse(localStorage.getItem("user"))
+
+
+    const handleLogout = ()=>{
+        navigator ('login')
+    }
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <Link className="navbar-brand" to='/'>SokoGarden</Link>
+            <button className="navbar-toggler" data-bs-toggle='collapse' data-bs-target='#navbarCollapse'>
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id='navbarCollapse'>
+                <div className="navbar-nav">
+                    <Link className="nav-link" to='/'>Home</Link>
+                    <Link className="nav-link" to='/addproduct'>Add Product</Link>
+                </div>
+
+
+
+                {user ? (<div className="navbar-nav ms-auto">
+                    <Link className="nav-link" to='#'>{user.username}</Link>
+                    <button className="btn btm-dark">Log Out</button>
+                </div>
+                ) : (<div className="navbar-nav ms-auto">
+                    <Link className="nav-link" to='/login'>Login</Link>
+                    <Link className="nav-link" to='/signup'>Sign Up</Link>
+                </div>
+                )}
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
+ort default navbarComponent;
